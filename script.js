d4428bc1ff2a509703ef4d61d56b3b13d0e972d1
Andika6679 <114057440+Andika6679@users.noreply.github.com>
2024-11-17 15:46:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-﻿document.getElementById('editButton').addEventListener('click', function() {
-    let followers = prompt("Masukkan jumlah pengikut:");
-    let following = prompt("Masukkan jumlah yang diikuti:");
-
-    if (followers !== null && following !== null) {
-        document.getElementById('followers').textContent = followers;
-        document.getElementById('following').textContent = following;
-    }
-});
+﻿
