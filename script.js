113986e79f9ba3f107c0adc57addafca5a591387
Andika6679 <114057440+Andika6679@users.noreply.github.com>
2024-11-17 15:33:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('editButton').addEventListener('click', function() {
+    let followers = prompt("Masukkan jumlah pengikut:");
+    let following = prompt("Masukkan jumlah yang diikuti:");
+
+    if (followers !== null && following !== null) {
+        document.getElementById('followers').textContent = followers;
+        document.getElementById('following').textContent = following;
+    }
+});
